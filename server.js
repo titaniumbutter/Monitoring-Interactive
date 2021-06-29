be385fe8980329a39fbfbb8c5b492ca881c5565a
titaniumbutter <israelfranco1998@gmail.com>
2021-06-29 13:29:38 -0500

    added APM

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require('express')
 const path = require('path')
 const app = express();
 
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: '9d44fef516bf474e87e5d3033cb1a72b',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
 app.use(express.json())
 
 app.get('/', function(req, res) {
+    rollbar.log('Hello world!')
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
-
 
 
 const port = process.env.PORT || 4545
